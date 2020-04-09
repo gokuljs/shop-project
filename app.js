3bc424b3bafb6b1fb83b92dcf3d205b846f9cf05
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-09 14:26:30 +0530

    building a shop app

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.listen(3000, function() {
+    console.log("port is starting at 3000");
+    console.log("server started")
+})
